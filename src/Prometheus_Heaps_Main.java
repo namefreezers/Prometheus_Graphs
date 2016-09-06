ad6010deb69d28049430599a1cb75ef3408790d3
Aleksandr <freezers@ukr.net>
2016-09-06 22:23:30 +0300

    almost done

--- src/Prometheus_Heaps_Main.java
+++ src/Prometheus_Heaps_Main.java
@@ -3,38 +3,78 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
 
-class Graph{
+class Graph {
 	ArrayList<HashSet<Integer>> list;
-	
-	Graph(String file){
+
+	Graph() {
+
+	}
+
+	Graph(String file) {
 		list = new ArrayList<HashSet<Integer>>();
 		try (BufferedReader in = new BufferedReader(new FileReader(file))) {
 			while (in.ready()) {
 				try (Scanner sc = new Scanner(in.readLine())) {
-					
+
 					int first = sc.nextInt(), second = sc.nextInt();
 					Set<Integer> set;
 					if ((set = list.get(first)) == null)
 						list.add(first, new HashSet<Integer>());
-					if (list.get(second) == null)
-						list.add(second, new HashSet<Integer>());
+					// if (list.get(second) == null)
+					// list.add(second, new HashSet<Integer>());
 					set.add(second);
 				}
-
 			}
 		} catch (IOException e) {
 			System.out.println("IOExc " + e);
 		}
+	}
+	
+	Graph reverse(){
+		Graph res = new Graph();
+		res.list = new ArrayList<HashSet<Integer>>();
+
+		for (Set<Integer> s : list)
+			for (Integer i : s) {
+				if (res.list.get(i) == null)
+					res.list.add(i, new HashSet<Integer>());
+				res.list.get(i).add(list.indexOf(s));
+			}
+		return res;
+	}
+
+	// NEDODELANO
+	void StronglyConnected() {
+		ArrayList<Integer> fin = new ArrayList<Integer>();
+		Graph g2 = reverse();
 		
+		
+	}
+
+	void DFSLoop(ArrayList<Integer> fin) {
+		Set<Integer> researched = new HashSet<Integer>();
+		for (Set<Integer> s : list)
+			if (!researched.contains(list.indexOf(s)))
+				DFSRUtilCon(list.indexOf(s), researched, -1, fin);
+
+	}
+
+	int DFSRUtilCon(int start, Set<Integer> researched, int t, ArrayList<Integer> fin) {
+		researched.add(start);
+		for (Integer i : list.get(start))
+			if (!researched.contains(i))
+				t = DFSRUtilCon(i, researched, t, fin);
+		t = t + 1;
+		fin.add(t, start);
+		return t;
 	}
 
 }
 
 public class Prometheus_Heaps_Main {
-	
+
 	public static void main(String[] args) {
 		Graph g = new Graph("test_08/test_08_1.txt");
-		
 
 	}
 
