0602fa4a46b3e9fa9013d960ead79acd9c045d9b
Aleksandr <freezers@ukr.net>
2016-09-06 21:26:56 +0300

    class Graph

--- src/Prometheus_Heaps_Main.java
+++ src/Prometheus_Heaps_Main.java
@@ -3,19 +3,21 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
 
-public class Prometheus_Heaps_Main {
-	static ArrayList<HashSet<Integer>> input(String file){
-		ArrayList<HashSet<Integer>> res = new ArrayList<HashSet<Integer>>();
+class Graph{
+	ArrayList<HashSet<Integer>> list;
+	
+	Graph(String file){
+		list = new ArrayList<HashSet<Integer>>();
 		try (BufferedReader in = new BufferedReader(new FileReader(file))) {
 			while (in.ready()) {
 				try (Scanner sc = new Scanner(in.readLine())) {
 					
 					int first = sc.nextInt(), second = sc.nextInt();
 					Set<Integer> set;
-					if ((set = res.get(first)) == null)
-						res.add(first, new HashSet<Integer>());
-					if (res.get(second) == null)
-						res.add(second, new HashSet<Integer>());
+					if ((set = list.get(first)) == null)
+						list.add(first, new HashSet<Integer>());
+					if (list.get(second) == null)
+						list.add(second, new HashSet<Integer>());
 					set.add(second);
 				}
 
@@ -23,11 +25,15 @@ public class Prometheus_Heaps_Main {
 		} catch (IOException e) {
 			System.out.println("IOExc " + e);
 		}
-		return res;
+		
 	}
 
+}
+
+public class Prometheus_Heaps_Main {
+	
 	public static void main(String[] args) {
-		ArrayList<HashSet<Integer>> list = input("test_08/test_08_1.txt");
+		Graph g = new Graph("test_08/test_08_1.txt");
 		
 
 	}
